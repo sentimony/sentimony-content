7b8b24fc3976f6fe76ff4c38b14cb7cd437a0113
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-02-22 02:46:39 +0200

    add vu3 styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,18 @@
 /**
+* Remodal
+* https://github.com/VodkaBears/Remodal
+*/
+$('.js-remodal').remodal({
+    // closeOnEscape: false
+});
+
+/**
 * EasyTabs
 * https://github.com/JangoSteve/jQuery-EasyTabs
 */
 $('.js-cashbox-tabs').easytabs({
-  animate: true,
-  animationSpeed: 100,
-  // tabs: '.cashbox-tabs__nav > ul > li',
-  tabActiveClass: 'selected',
+    animate: true,
+    animationSpeed: 100,
+    // tabs: '.cashbox-tabs__nav > ul > li',
+    tabActiveClass: 'selected',
 });
