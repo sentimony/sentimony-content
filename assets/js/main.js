f023b923e0b969f3a9294ef0951bb0948f3d0c1f
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-02-25 01:56:19 +0200

    add updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,9 +17,44 @@ $('.js-easytabs').easytabs({
 });
 
 /**
-* exchange__label--selected
+* include throw
 */
-$('.exchange__label').click(function() {
-    $('.exchange__label').removeClass('exchange__label--selected');
-    $(this).toggleClass('exchange__label--selected');
-});
+// rub2fun
+$(".js-rub2fun-rub").keyup(function () {
+  var value = $(this).val();
+  $(".js-rub2fun-fun").val(value);
+  $(".js-rub2fun-result").text(value);
+}).keyup();
+ 
+$(".js-rub2fun-fun").keyup(function () {
+  var value = $(this).val();
+  $(".js-rub2fun-rub").val(value);
+  $(".js-rub2fun-result").text(value);
+}).keyup();
+
+// cp2fun
+$(".js-cp2fun-cp").keyup(function () {
+  var value = $(this).val();
+  $(".js-cp2fun-fun").val(value);
+  $(".js-cp2fun-result").text(value);
+}).keyup();
+ 
+$(".js-cp2fun-fun").keyup(function () {
+  var value = $(this).val();
+  $(".js-cp2fun-cp").val(value);
+  $(".js-cp2fun-result").text(value);
+}).keyup();
+
+/**
+* calculation
+*/
+// var number1 = $(".js-number1").val();
+// var number2 = $(".js-number2").val();
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// var result = multiply(number1, number2);
+
+// $(".js-result").text(result)
