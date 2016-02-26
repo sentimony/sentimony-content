358be583d7c62ed15f2c142616fea63e1cb924a0
Your NameIgor <igorztsorokopud@gmail.com>
2016-02-26 12:50:36 +0200

    Fixed Exchange Points on Fun Js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,35 +17,20 @@ $('.js-easytabs').easytabs({
     updateHash: false
 });
 
-/**
- * include throw
- */
-// rub2fun
-$(".js-rub2fun-rub").keyup(function () {
-    var value = $(this).val();
-    $(".js-rub2fun-fun").val(value);
-    $(".js-rub2fun-result").text(value);
-}).keyup();
+/* Exchange Points on Fun */
+$('.js-cp2fun-cp').keyup(function () {
+    var value = $(this).val(),
+        resultSum = value * 1.25;
+    $('.js-cp2fun-fun').val(resultSum);
+    $(".js-cp2fun-result").text(resultSum);
+});
 
-$(".js-rub2fun-fun").keyup(function () {
-    var value = $(this).val();
-    $(".js-rub2fun-rub").val(value);
-    $(".js-rub2fun-result").text(value);
-}).keyup();
-
-// cp2fun
-$(".js-cp2fun-cp").keyup(function () {
-    var value = $(this).val();
-    $(".js-cp2fun-fun").val(value);
+$('.js-cp2fun-fun').keyup(function () {
+    var value = $(this).val(),
+        resultSum = value / 1.25;
+    $('.js-cp2fun-cp').val(resultSum);
     $(".js-cp2fun-result").text(value);
-}).keyup();
-
-$(".js-cp2fun-fun").keyup(function () {
-    var value = $(this).val();
-    $(".js-cp2fun-cp").val(value);
-    $(".js-cp2fun-result").text(value);
-}).keyup();
-
+});
 
 /* Checked cashbox */
 $('.js-exchange').click(function () {
@@ -92,16 +77,3 @@ function notIncrease(element, ownSum) {
 notIncrease('.js-exchange .exchange__input-text', $('.js-exchange-rub').text().match(/\d+/)[0]);
 notIncrease('.exchange__item--noradio .exchange__input-text', $('.js-exchange-points').text().match(/\d+/)[0]);
 
-/**
- * calculation
- */
-// var number1 = $(".js-number1").val();
-// var number2 = $(".js-number2").val();
-
-// function multiply(a, b) {
-//   return a * b;
-// }
-
-// var result = multiply(number1, number2);
-
-// $(".js-result").text(result)
