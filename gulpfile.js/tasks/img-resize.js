7200be72ed0ca14bcd1f3a1b661fb05db7d30b68
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-03-24 00:48:36 +0200

    add sombrero-red

--- gulpfile.js/tasks/img-resize.js
+++ gulpfile.js/tasks/img-resize.js
@@ -1,22 +1,22 @@
 var gulp = require('gulp'),
-    imageResize = require('gulp-image-resize');
+  imageResize = require('gulp-image-resize');
 
 gulp.task('img-resize-small', function () {
-    gulp.src('assets/img/releases/large/**/*.*')
-        .pipe(imageResize({ 
-            width : 120,
-            height : 120,
-            upscale: true
-        }))
-        .pipe(gulp.dest('assets/img/releases/small/'));
+  gulp.src('assets/img/releases/large/**/*.*')
+    .pipe(imageResize({ 
+      width : 120,
+      height : 120,
+      upscale: true
+    }))
+    .pipe(gulp.dest('assets/img/releases/small/'));
 });
 
 gulp.task('img-resize-medium', function () {
-    gulp.src('assets/img/releases/large/**/*.*')
-        .pipe(imageResize({ 
-            width : 600,
-            height : 600,
-            upscale: true
-        }))
-        .pipe(gulp.dest('assets/img/releases/medium/'));
+  gulp.src('assets/img/releases/large/**/*.*')
+    .pipe(imageResize({ 
+      width : 600,
+      height : 600,
+      upscale: true
+    }))
+    .pipe(gulp.dest('assets/img/releases/medium/'));
 });
