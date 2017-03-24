d6008cd69644afa9f18a5bb1a5510812ad8366c9
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2017-03-24 23:36:22 +0200

    add new releases resize gulp tasks

--- gulpfile.js/tasks/img-resize.js
+++ gulpfile.js/tasks/img-resize.js
@@ -11,12 +11,43 @@ gulp.task('img-resize-small', function () {
     .pipe(gulp.dest('assets/img/releases/small/'));
 });
 
+gulp.task('img-resize-small-retina', function () {
+  gulp.src('assets/img/releases/large/**/*.*')
+    .pipe(imageResize({ 
+      width : 240,
+      height : 240,
+      upscale: true
+    }))
+    .pipe(gulp.dest('assets/img/releases/small-retina/'));
+});
+
 gulp.task('img-resize-medium', function () {
   gulp.src('assets/img/releases/large/**/*.*')
     .pipe(imageResize({ 
-      width : 600,
-      height : 600,
+      width : 190,
+      height : 190,
       upscale: true
     }))
     .pipe(gulp.dest('assets/img/releases/medium/'));
+});
+
+gulp.task('img-resize-medium-retina', function () {
+  gulp.src('assets/img/releases/large/**/*.*')
+    .pipe(imageResize({ 
+      width : 380,
+      height : 380,
+      upscale: true
+    }))
+    .pipe(gulp.dest('assets/img/releases/medium-retina/'));
+});
+
+gulp.task('img-resize-og-images', function () {
+  gulp.src('assets/img/releases/large/**/*.*')
+    .pipe(imageResize({ 
+      width : 526,
+      height : 274,
+      upscale: true,
+      crop: true
+    }))
+    .pipe(gulp.dest('assets/img/releases/og-images/'));
 });
